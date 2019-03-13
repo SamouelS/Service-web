6080379c193b102bb19b608497f32e6f3ee29bd6
SamouelS <samuel.pacault@gmail.com>
2019-03-13 14:12:37 +0100

    route

--- app/src/routes.php
+++ app/src/routes.php
@@ -5,17 +5,16 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{table:patient|personne}]', function (Request $request, Response $response, array $args) {
-    $sqlRequest = 'SELECT * FROM '.$args['table'];
+$app->get('/[{table:patients|personnes|badges|infirmieres|visites}]', function (Request $request, Response $response, array $args) {
+    $sqlRequest = 'SELECT * FROM '.preg_replace('/s$/', '', $args['table']);
     $retour = $this->db->query($sqlRequest);
     $json = array();
     foreach ($retour as $row) {
         $json[] = json_encode($row);
     }
     $json = json_encode($json);
-    var_dump($json);
-
+    return $json;
+    //var_dump($json);
     //var_dump($sqlRequest);
-
-
 });
+
